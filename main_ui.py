d47085999bdf15f18a7ae1a17120a236f6580259
JoseSanta98 <mibox.santa@gmail.com>
2026-02-17 13:52:54 -0600

    Agregar función pura puede_reabrir_caja

--- main_ui.py
+++ main_ui.py
@@ -35,6 +35,10 @@ def puede_cerrar_caja(estado_caja):
     return estado_caja == ESTADO_ABIERTA
 
 
+def puede_reabrir_caja(estado_caja):
+    return estado_caja == ESTADO_CERRADA
+
+
 def calcular_peso_caja(contenido):
     return sum(p['peso'] for p in contenido)
 
